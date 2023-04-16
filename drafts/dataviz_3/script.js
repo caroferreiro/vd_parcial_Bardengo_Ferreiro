6fdf0d00cfbb4ca27f8acafd1581b6df0daca858
santinabardengo <santibardengo@gmail.com>
2023-04-16 11:16:41 -0300

    commit 11

--- drafts/dataviz_3/script.js
+++ drafts/dataviz_3/script.js
@@ -1,37 +1,39 @@
-// const data_ene = d3.dsv( ';','/data/dataset.csv', d3.autoType )
-// const data_dic = d3.dsv(';', '/data/dataset_dic.csv', d3.autoType)
 
-
-// Promise.all([data_ene, data_dic]).then(([enero, diciembre]) => {
-//     let chart = Plot.plot({
-//         marks: [
-//             Plot.line(enero,diciembre, 
-            
-//             )
-//         ]
-//     })
-
-// })
-
-d3.csv('/data/dataset.csv', d3.autoType).then(data_ene => {
-    d3.csv('/data/dataset_dic.csv', d3.autoType).then(data_dic => {
+// d3.csv('/data/dataset.csv', d3.autoType).then(data_ene => {
+//     d3.csv('/data/dataset_dic.csv', d3.autoType).then(data_dic => {
       
-      // Combinar los dos conjuntos de datos en uno solo
-    const data = data_ene.map((d, i) => Object.assign({}, d, data_dic[i]));
-    
-    let casos_abiertos = data.filter(
-      d => d.estado_del_contacto == 'Abierto',
-    )
+//     // Combinar los dos conjuntos de datos en uno solo
+//     const data = data_ene.map((d, i) => Object.assign({}, d, data_dic[i]));
+//     console.log(data)
+//     let casos_abiertos = data.filter(
+//       d => d.estado_del_contacto == 'Abierto',
+//     )
   
-      // Configurar la visualización
-      const chart = Plot.plot({
-        marks: [
-          Plot.line(casos_abiertos, )
-        ]
-      });
+//       // Configurar la visualización
+//       const chart = Plot.plot({
+//         marks: [
+//           Plot.line(casos_abiertos, )
+//         ]
+//       });
       
-    });
-  });
+//     });
+//   });
+
+Promise.all([
+  d3.csv('/data/dataset.csv', d3.autoType),
+  d3.csv('/data/dataset_dic.csv', d3.autoType)
+  ]).then(([data_ene, data_dic]) => {
+  
+  // Combinar los dos conjuntos de datos en uno solo
+  const data = d3.merge([data_ene, data_dic]);
+
+  let casos_abiertos = data.filter(d => d.estado_del_contacto == 'Abierto');
+
+
+  // Realiza un plot con el nuevo conjunto de datos
+  // ...
+  
+});
 
   
   
