e0b9d4ced8bb2120ebf2f0b0cb0a8f92dccdfe71
Caro Ferreiro <carolina.ferreiro@mail.utdt.edu>
2023-04-16 15:03:14 -0300

    o

--- drafts/dataviz_3/script.js
+++ drafts/dataviz_3/script.js
@@ -19,21 +19,48 @@
 //     });
 //   });
 
-Promise.all([
-  d3.csv('/data/dataset.csv', d3.autoType),
-  d3.csv('/data/dataset_dic.csv', d3.autoType)
-  ]).then(([data_ene, data_dic]) => {
-  
+dataEnero = d3.dsv(';', '/data/dataset.csv', d3.autoType)
+dataDiciembre = d3.dsv(';', '/data/dataset_dic.csv', d3.autoType)
+
+Promise.all([dataEnero, dataDiciembre]).then(([data_ene, data_dic]) => {
   // Combinar los dos conjuntos de datos en uno solo
   const data = d3.merge([data_ene, data_dic]);
-
   let casos_abiertos = data.filter(d => d.estado_del_contacto == 'Abierto');
+  let chart = Plot.plot({
+    marks: [
+      Plot.ruleX([1]),
+      Plot.line(data_ene, Plot.groupY({x:'sum'}, {
+        y: 'estado_del_contacto',
+      })),
+      Plot.line(data_dic, Plot.groupY({x:'sum'}, {
+        y: 'estado_del_contacto',
+      })),
+      // Plot.axisX({
+      //   label: 'Días que tardó en resolverse →',
+      //   labelOffset: 35,
+      //   ticks: 8,
+      // }),
+      // Plot.axisY({
+      //   label: 'Cantidad de denuncias ↑',
+      //   labelOffset: 27,
+      //   ticks: 6,
+      // }),
+    ],
+    color: {
+      scheme: 'gnbu',
+    },
+    width: 400,
+    height: 200,
+    insetLeft: 10,
+    insetRight: 10,
+    insetTop: 10,
+    marginBottom: 40,
+    marginLeft: 60,
+    })
+    d3.select('#chart').append(() => chart)
+})
 
 
-  // Realiza un plot con el nuevo conjunto de datos
-  // ...
-  
-});
 
   
   
