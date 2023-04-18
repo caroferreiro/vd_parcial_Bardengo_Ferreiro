38789ce9d234e8f9022d8485de093f0466aff401
Caro Ferreiro <carolina.ferreiro@mail.utdt.edu>
2023-04-18 13:55:29 -0300

    nnnnnnnnnnnn

--- drafts/dataviz_3/script.js
+++ drafts/dataviz_3/script.js
@@ -25,32 +25,63 @@ dataDiciembre = d3.dsv(';', '/data/dataset_dic.csv', d3.autoType)
 Promise.all([dataEnero, dataDiciembre]).then(([data_ene, data_dic]) => {
   // Combinar los dos conjuntos de datos en uno solo
   const data = d3.merge([data_ene, data_dic]);
-  let casos_abiertos = data.filter(d => d.estado_del_contacto == 'Abierto');
+  var parseTime = d3.timeParse('%d/%m/%Y');
+  data.forEach(function(d) {
+  d.fecha_ingreso = parseTime(d.fecha_ingreso);
+});
+
+var formatMonth = d3.timeFormat('%m');
+  data.forEach(function(d) {
+  d.mes = formatMonth(d.fecha_ingreso);
+});
+
+var formatDay = d3.timeFormat('%d');
+  data.forEach(function(d) {
+  d.dia = formatDay(d.fecha_ingreso);
+});
+
+  let enero = data.filter(d => d.mes == '01');
+  let dic = data.filter(d => d.mes == '12');
+  console.log(enero.filter(d => d.estado_del_contacto == 'Abierto'))
   let chart = Plot.plot({
     marks: [
-      Plot.ruleX([1]),
-      Plot.line(data_ene, Plot.groupY({x:'sum'}, {
-        y: 'estado_del_contacto',
+      Plot.ruleX([0]),
+      Plot.ruleY([0]),
+      Plot.line(enero.filter(d => d.estado_del_contacto == 'Abierto'), Plot.binX({y:'sum'} ,{
+        x: 'dia',
+        stroke: 'mes',
+        strokeWidth: 3.2,
       })),
-      Plot.line(data_dic, Plot.groupY({x:'sum'}, {
-        y: 'estado_del_contacto',
+      // Plot.line(enero.filter(d => d.estado_del_contacto == 'Cerrado'), Plot.binX({y:'sum'} ,{
+      //   x: 'dia',
+      // })),
+      Plot.line(dic.filter(d => d.estado_del_contacto == 'Abierto'), Plot.binX({y:'count'}, {
+        x: 'dia',
+        stroke: 'mes',
+        strokeWidth: 3.2,
       })),
-      // Plot.axisX({
-      //   label: 'Días que tardó en resolverse →',
-      //   labelOffset: 35,
-      //   ticks: 8,
-      // }),
-      // Plot.axisY({
-      //   label: 'Cantidad de denuncias ↑',
-      //   labelOffset: 27,
-      //   ticks: 6,
-      // }),
+      // Plot.line(dic.filter(d => d.estado_del_contacto == 'Cerrado'), Plot.binX({y:'sum'}, {
+      //   x: 'dia',
+      // })),
+      Plot.axisX({
+        label: 'Días del mes →',
+        labelOffset: 35,
+        ticks: 20,
+      }),
+      Plot.axisY({
+        label: 'Cantidad de denuncias abiertas ↑',
+        labelOffset: 27,
+        ticks: 6,
+      }),
     ],
-    color: {
-      scheme: 'gnbu',
+    x: {
+      domain: [1, 31],
     },
-    width: 400,
-    height: 200,
+    color: {
+      range: ['YellowGreen', 'CornflowerBlue'],
+    },
+    width: 500,
+    height: 300,
     insetLeft: 10,
     insetRight: 10,
     insetTop: 10,
